68e3dd86e5c56acd5f7cfe1f345e9984821f788f
Joran Bleeker <joran@DESKTOP-8O6GTB8.localdomain>
2021-03-15 09:18:15 +0000

    refactor: use iterator instead of list in resolve methods

--- calculation/calculation.py
+++ calculation/calculation.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Callable, NewType
+from typing import List, Tuple, Iterator, Callable, NewType
 
 OPERATORS = {
     '+': lambda x, y: x + y,
@@ -11,13 +11,13 @@ OPERATORS = {
 def resolve_calculation(calculation: str, notation: str = 'prefix') -> int:
     calculation_elements = calculation.split()
     resolve_method = get_resolve_method(notation)
-    result, _ = resolve_method(calculation_elements)
+    result = resolve_method(iter(calculation_elements))
     return result
 
 class UnknownNotation(Exception):
     pass
 
-def get_resolve_method(notation: str) -> Callable[[List[str]], Tuple[int, List[str]]]:
+def get_resolve_method(notation: str) -> Callable[[Iterator[str]], int]:
     if notation == 'prefix':
         return resolve_prefix_calculation
     if notation == 'infix':
@@ -25,20 +25,20 @@ def get_resolve_method(notation: str) -> Callable[[List[str]], Tuple[int, List[s
     raise UnknownNotation(notation)
 
 
-def resolve_prefix_calculation(calculation_elements: List[str]) -> Tuple[int, List[str]]:
-    next_element, *rest = calculation_elements
+def resolve_prefix_calculation(calculation_iter: Iterator[str]) -> int:
+    next_element = next(calculation_iter)
     if next_element in OPERATORS:
-        first_operand, rest = resolve_prefix_calculation(rest)    
-        second_operand, rest = resolve_prefix_calculation(rest)
-        return OPERATORS[next_element](first_operand, second_operand), rest
-    return int(next_element), rest
+        first_operand = resolve_prefix_calculation(calculation_iter)    
+        second_operand = resolve_prefix_calculation(calculation_iter)
+        return OPERATORS[next_element](first_operand, second_operand)
+    return int(next_element)
 
-def resolve_infix_calculation(calculation_elements: List[str]) -> Tuple[int, List[str]]:
-    next_element, *rest = calculation_elements
+def resolve_infix_calculation(calculation_iter: Iterator[str]) -> int:
+    next_element = next(calculation_iter)
     if next_element == '(':
-        first_operand, rest = resolve_infix_calculation(rest)
-        operator, *rest = rest
-        second_operand, rest = resolve_infix_calculation(rest)
-        closing_bracket, *rest = rest
-        return OPERATORS[operator](first_operand, second_operand), rest
-    return int(next_element), rest
+        first_operand = resolve_infix_calculation(calculation_iter)
+        operator = next(calculation_iter)
+        second_operand = resolve_infix_calculation(calculation_iter)
+        closing_bracket = next(calculation_iter)
+        return OPERATORS[operator](first_operand, second_operand)
+    return int(next_element)
